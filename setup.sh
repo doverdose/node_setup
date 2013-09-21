fb901f4efaec0e06519692976069d2502c8f65c1
doverdose <royce.cheng@gmail.com>
2013-09-21 18:09:40 -0400

    Added node-gyp install step

--- setup.sh
+++ setup.sh
@@ -8,10 +8,11 @@ sudo apt-get install -y git
 sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
+# Load nvm and install latest production node, as well as node-gyp
 source $HOME/.nvm/nvm.sh
 nvm install v0.10.12
 nvm use v0.10.12
+npm install -g node-gyp
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
