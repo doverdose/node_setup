a9cb6dbeb8d9c350ea97115940b3c652e8c5ec59
doverdose <royce.cheng@gmail.com>
2013-11-15 10:35:52 -0500

    Install tmux

--- setup.sh
+++ setup.sh
@@ -34,6 +34,10 @@ sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
+
+# Install tmux
+sudo apt-get install -y tmux
+
 # git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
